3fd21dcf94959da0c7e661b855d38286c23d23a6
kevin kibebe <kibebekevin@gmail.com>
2022-11-21 23:51:34 +0300

    updated

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -3,7 +3,7 @@ import django
 from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studirre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 django.setup()
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
